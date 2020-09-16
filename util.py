3497610bd791cc12b22011d7e4af95ec6ce34708
unknown <mehraj24158@gmail.com>
2020-09-16 18:05:03 -0400

    testing

--- util.py
+++ util.py
@@ -1,7 +1,32 @@
+from abc import ABC, abstractmethod
+
+class User(ABC):
+    """
+    Base Class for Technicians, Residents, and Admins:
+
+    Name: First and last name separated by a space
+    ID: Numerical number, 8 digits long
+    History: Stores history of associated ticket ID's
+    """
+    def __init__(self, name, id, phone_number, email):
+        self.name = name
+        self.id = id
+        self.phone_number = phone_number
+        self.email = email
+
+class person(User):
+    def __init__():
+        super(person, self).__init__()
+
+        
+
+
+
+
 class Technician:
     """
-    Testing Github Push
         Technician Class:
+
         Name: Technician first and last name separated by a space
         ID: Numerical employment number, 8 digits long
         Credentials: List of degrees or certifications
@@ -12,10 +37,16 @@ class Technician:
     def __init__(self, name, tech_id, credentials=None, salary=None):
         self.name = name
         self.id = tech_id
+        self.phone_number = phone_number
+        self.email = email
+
+
         self.credentials = credentials
         self.history = []
         self.salary = salary
         self.bonus = None
+
+
 
     def bonus(self, bonus):
         self.bonus = bonus
